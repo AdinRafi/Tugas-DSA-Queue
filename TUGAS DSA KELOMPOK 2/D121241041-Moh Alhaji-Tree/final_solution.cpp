b09325559a417faab74749d8a8e967402c458a98
JixawsMaeril <moh.alhaji74@gmail.com>
2025-05-13 12:38:19 +0800

    final_solution.cpp

---
+++ TUGAS DSA KELOMPOK 2/D121241041-Moh Alhaji-Tree/final_solution.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree = Konsep Struktur Data yang terdiri atas nodes(simpul) atau daun(leaf) dan akar(root)
+// Bersifat Hierarki
+// Height = Jumlah sisi dari root sampai leaf yang terjauh.
+
+//Program ini hanya memerlukan Operasi Menghitung Tinggi Sebuah Tree
+
+class Node{ //Untuk meyimpan data(nilai) untuk digunakan dalam Tree
+    public: //Agar Class bisa diakses di semua program
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){ //Ketika nilai dimasukkan, d akan menjadi root pertama, dan anakannya (left dan right) akan menjadi NULL atau kosong
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution{ //Untuk menginput data(nilai) dan menghitung tinggi dari sebuah Tree
+    public:
+        Node* insert(Node* root, int data){ //Merekursif hasil inputan sebagai data, node left, atau node right
+            if(root == NULL){
+                return new Node(data);
+            }else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+                return root;
+            }
+        }
+
+    int height(Node* root){
+        if(root == NULL) return -1;
+        return max(height(root->left), height(root->right)) + 1;
+    }
+};
+
+
+int main(){
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0) {
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	cout << height;
+
+    return 0;
+}
