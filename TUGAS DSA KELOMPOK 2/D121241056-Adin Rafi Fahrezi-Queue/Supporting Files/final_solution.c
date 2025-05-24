1d6c2e00ad3cad5eaabef59ee204279fd5900508
AdinRafi <adinrafifahrezi@gmail.com>
2025-05-24 17:34:42 +0800

    Added new file

--- TUGAS DSA KELOMPOK 2/D121241056-Adin Rafi Fahrezi-Queue/Supporting Files/final_solution.c
+++ TUGAS DSA KELOMPOK 2/D121241056-Adin Rafi Fahrezi-Queue/Supporting Files/final_solution.c
@@ -1,79 +1,99 @@
 #include <stdio.h>
 
-#define LIMIT 1000001 // Batas Nilai Array + 1
+/*Dokumentasi Program
 
-// Fungsi untuk mengecek isi deque
-int dequeNotEmpty(int front, int back)
+Fungsi Program
+Mencari nilai minimum dari maksimum subarray sesuai ukuran subarray(query[q]) dengan sistem window
+menggunakan deque sebagai indeks dari data(arrr[i]) yang memiliki 4 operasi dasar:
+1. Memajukan indeks depan deque jika melewati ukuran subarray
+2. Menghapus elemen dari indeks belakang deque jika isi elemen kurang dari elemen saat ini
+3. Mencari nilai maksimum dari subarray
+4. Mencari nilai minimum dari semua maksimum subarray*/
+
+#define MAKS_N 100000      // Batas n dan query[q] (1 ≤ n ≤ 10⁵ dan 1 ≤ d ≤ n)
+#define MAKS_Q 100         // Batas q (1 ≤ q ≤ 100)
+#define MAKS_NILAI 1000000 // Batas arr[1] + 1 (0 ≤ arr[i] < 10⁶)
+
+void bacaArray(int array[], int jumlah)
 {
-    return front <= back; // Mengembalikan true apabila deque tidak kosong dan false jika sebaliknya
+    for (int i = 0; i < jumlah; i++)
+    {
+        scanf("%d", &array[i]);
+    }
 }
 
-// Fungsi untuk mencari nilai minimal dari maximal sub-array deque sesuai panjang k
-int minOfMaxInSubarrays(const int *arr, int n, int k)
+int dequeKosong(int deque[], int indeksDepan, int indeksBelakang)
 {
-    int deque[n];             // deque sebagai indeks pada array dengan batas n
-    int front = 0, back = -1; // Inisialisasi front dan back
+    return indeksDepan > indeksBelakang;
+}
 
-    int minOfMAX = LIMIT; // Deklarasi minOfMAX dengan nilai LIMIT
+int cariMinimumDariSemuaMaksimumSubarray(int data[], int jumlahData, int ukuranSubarray)
+{
+    int deque[MAKS_N];
+    int indeksDepan = 0;
+    int indeksBelakang = -1;
 
-    for (int i = 0; i < n; i++) // Loop untuk menentukan nilai minimal dari maximal sub-array deque
+    int hasilMinimum = MAKS_NILAI;
+
+    for (int i = 0; i < jumlahData; i++)
     {
-        if (dequeNotEmpty(front, back) && deque[front] <= i - k) // Memajukan indeks apabila sudah memenuhi kueri(k) / keluar dari window
+        if (!dequeKosong(deque, indeksDepan, indeksBelakang) && deque[indeksDepan] <= i - ukuranSubarray)
         {
-            front++;
+            indeksDepan++;
         }
 
-        while (dequeNotEmpty(front, back) && arr[i] >= arr[deque[back]]) // Menghapus elemen yang lebih kecil dari array[i] dari belakang
+        while (!dequeKosong(deque, indeksDepan, indeksBelakang) && data[i] >= data[deque[indeksBelakang]])
         {
-            back--;
+            indeksBelakang--;
         }
 
-        deque[++back] = i; // Menambahkan nilai indeks pada deque
+        deque[++indeksBelakang] = i;
 
-        if (i >= k - 1) // Menaruh nilai minimal apabila currentMAX kurang dari minOfMAX
+        if (i >= ukuranSubarray - 1)
         {
-            int currentMAX = arr[deque[front]];
-            if (minOfMAX > currentMAX)
-                minOfMAX = currentMAX;
+            int nilaiMaksimum = data[deque[indeksDepan]];
+            if (nilaiMaksimum < hasilMinimum)
+            {
+                hasilMinimum = nilaiMaksimum;
+            }
         }
     }
 
-    return minOfMAX; // Mengembalikan nilai minimal dari maximal sub-array deque
+    return hasilMinimum;
 }
 
-void solve(int *arr, int *query, int *result, int n, int q)
+void prosesSemuaPertanyaan(int data[], int jumlahData, int listUkuranSubarray[], int jumlahKueri, int hasil[])
 {
-    for (int i = 0; i < q; i++)
+    for (int i = 0; i < jumlahKueri; i++)
     {
-        result[i] = minOfMaxInSubarrays(arr, n, query[i]); // Mengisi nilai result pada indeks i sesuai hasil fungsi minOfMaxInSubarrays
+        hasil[i] = cariMinimumDariSemuaMaksimumSubarray(data, jumlahData, listUkuranSubarray[i]);
+    }
+}
+
+void cetakHasil(int hasil[], int jumlah)
+{
+    for (int i = 0; i < jumlah; i++)
+    {
+        printf("%d\n", hasil[i]);
     }
 }
 
 int main()
 {
-    int n, q;               // Inisialisasi n dan q
-    scanf("%d %d", &n, &q); // Mengisi nilai n dan q sesuai input
+    int jumlahData, jumlahKueri;
 
-    int arr[n];    // Array sebagai elemen dengan batas n
-    int query[q];  // Kueri sebagai tempat menyimpan kueri(k) dengan batas n
-    int result[q]; // Result sebagai tempat menyimpan hasil dengan batas n
+    scanf("%d %d", &jumlahData, &jumlahKueri);
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]); // Mengisi nilai array pada indeks i sesuai input
-    }
+    int data[MAKS_N];
+    int listUkuranSubarray[MAKS_Q];
+    int hasil[MAKS_Q];
 
-    for (int i = 0; i < q; i++)
-    {
-        scanf("%d", &query[i]); // Mengisi nilai query pada indeks i sesuai input
-    }
+    bacaArray(data, jumlahData);
+    bacaArray(listUkuranSubarray, jumlahKueri);
 
-    solve(arr, query, result, n, q); // Memanggil fungsi solve untuk memecahkan masalah
+    prosesSemuaPertanyaan(data, jumlahData, listUkuranSubarray, jumlahKueri, hasil);
 
-    for (int i = 0; i < q; i++)
-    {
-        printf("%d\n", result[i]); // Menampilkan nilai result pada indeks i
-    }
+    cetakHasil(hasil, jumlahKueri);
 
     return 0;
 }
